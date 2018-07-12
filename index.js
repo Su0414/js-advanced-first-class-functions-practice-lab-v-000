3fef5ba9d4c339279b3b3eff4874a0995b51606d
Suchitra Naik <susanaik@gmail.com>
2018-07-12 15:09:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,5 @@ const totalRevenue = function (drivers){
 
 
 const averageRevenue = function (drivers){
-  return drivers.reduce(function(total, driverName){
-    return (driverName.revenue + total)/drivers.length
-  }, 0);
+    return totalRevenue(drivers)/drivers.length
 };
